3a22211237d84b3eaa2361b4e565b36f53cd77f9
Panem <titoluccao@gmail.com>
2022-04-02 23:39:44 -0300

    feat: update storage and config separation

--- src/Config.ts
+++ src/Config.ts
@@ -1,38 +1,12 @@
-import { config as dotenv } from 'dotenv'
-import type { FirebaseOptions } from 'firebase/app'
-
 export default class Config {
 
-    config!: Configuration
+    private static _instance: Config
 
-    constructor() {
-        this.setupDotenv()
-        this.verifyKeys()
-        this.setupConfig()
+    private constructor() {}
+
+    static getInstance(): Config {
+        if (!this._instance) this._instance = new Config()
+        return this._instance
     }
 
-    async setupDotenv() {
-        if (process.env.NODE_ENV === 'development') dotenv()
-    }
-
-    async verifyKeys() {
-        if (!process.env.FIREBASE_CONFIG) {
-            throw new Error('Firebase settings not found in FIREBASE_CONFIG')
-        }
-    }
-
-    async setupConfig() {
-        this.config = {
-            firebaseConfig: JSON.parse(process.env.FIREBASE_CONFIG)
-        }
-    }
-
-    getConfig(): Configuration {
-        return this.config
-    }
-
-}
-
-export interface Configuration {
-    firebaseConfig: FirebaseOptions
 }
--- src/Storage.ts
+++ src/Storage.ts
@@ -1,20 +1,50 @@
-import type { FirebaseApp } from 'firebase/app'
+import { config as dotenv } from 'dotenv'
+import type { FirebaseApp, FirebaseOptions } from 'firebase/app'
 import { initializeApp } from 'firebase/app'
+import { Firestore, getFirestore } from 'firebase/firestore/lite'
 
 export default class Storage {
 
     private static _instance: Storage
-    app: FirebaseApp
+    private app!: FirebaseApp
+    private db!: Firestore
 
     private constructor() {
-        this.app = initializeApp({
-
-        }, 'caputao-comunismo')
+        this.createApp(this.getDbConfig())
+        this.createFirestore()
     }
 
-    static getInstance() {
+    static getInstance(): Storage {
         if (!this._instance) this._instance = new Storage()
         return this._instance
     }
 
+    getApplication(): FirebaseApp {
+        return this.app
+    }
+
+    getDatabase(): Firestore {
+        return this.db
+    }
+
+    private getDbConfig(): FirebaseOptions {
+        if (process.env.NODE_ENV === 'development') dotenv()
+        if (!process.env.FIREBASE_CONFIG)
+            throw new Error('Firebase config not found')
+
+        return JSON.parse(process.env.FIREBASE_CONFIG)
+    }
+
+    private async createApp(config: FirebaseOptions): Promise<void> {
+        this.app = initializeApp(config, 'caputao-comunismo')
+    }
+
+    private async createFirestore(): Promise<void> {
+        this.db = getFirestore(this.app)
+    }
+
+}
+
+export interface Configuration {
+    BOT_TOKEN: string
 }
