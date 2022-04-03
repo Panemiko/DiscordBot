e0277e1e26172bddab9b7c16424292e7d2fe5c88
Panem <titoluccao@gmail.com>
2022-04-03 13:04:42 -0300

    feat: add Bot.login to Application.run

--- src/Application.ts
+++ src/Application.ts
@@ -2,14 +2,12 @@ import Bot from './Bot'
 import Storage from './Storage'
 
 export default class Application {
-
     constructor() {
         Bot.getInstance()
         Storage.getInstance()
     }
 
     async run(): Promise<void> {
-
+        await Bot.getInstance().login()
     }
-
 }
