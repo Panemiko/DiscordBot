82c08723025aac71d17f6fd11ca511e89c5bbf26
Panem <titoluccao@gmail.com>
2022-04-03 13:04:07 -0300

    feat: add Bot.login method

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,9 +1,12 @@
-export default class Bot {
+import { Client, Intents } from 'discord.js'
+import Config from './Config'
 
+export default class Bot {
     private static instance: Bot
+    client!: Client
 
     private constructor() {
-
+        this.createClient()
     }
 
     static getInstance(): Bot {
@@ -11,4 +14,15 @@ export default class Bot {
         return this.instance
     }
 
+    async login(): Promise<void> {
+        const config = await Config.getInstance().getConfig()
+
+        await this.client.login(config.BOT_TOKEN)
+    }
+
+    private async createClient(): Promise<void> {
+        this.client = new Client({
+            intents: new Intents(32767),
+        })
+    }
 }
