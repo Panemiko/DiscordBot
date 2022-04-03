6394cdadc69fdeab00619a16a9aced14ca186e27
Panem <titoluccao@gmail.com>
2022-04-03 13:51:58 -0300

    refatore: update Bot.instance to Bot._instance

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,7 +2,7 @@ import { Client, Intents } from 'discord.js'
 import Config from './Config'
 
 export default class Bot {
-    private static instance: Bot
+    private static _instance: Bot
     client!: Client
 
     private constructor() {
@@ -10,8 +10,8 @@ export default class Bot {
     }
 
     static getInstance(): Bot {
-        if (!this.instance) this.instance = new Bot()
-        return this.instance
+        if (!this._instance) this._instance = new Bot()
+        return this._instance
     }
 
     async login(): Promise<void> {
