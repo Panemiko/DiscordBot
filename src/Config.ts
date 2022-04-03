c6143aed25a24e59b5267e06de933aff3b1494a5
Panem <titoluccao@gmail.com>
2022-04-03 11:42:14 -0300

    fix: Config.getConfig method

--- src/Config.ts
+++ src/Config.ts
@@ -1,22 +1,18 @@
 import Storage from './Storage'
 
 export default class Config {
-
     private static _instance: Config
 
-    private constructor() { }
+    private constructor() {}
 
     static getInstance(): Config {
         if (!this._instance) this._instance = new Config()
         return this._instance
     }
 
-    async getConfig(): Promise<Configuration> {
-        const storage = Storage.getInstance()
-        const collection = await storage.getCollection('settings')
-        return await storage.getDocuments<Configuration>(collection)[0]
+    async getConfig() {
+        return await Storage.getInstance().query<Configuration>('settings')
     }
-
 }
 
 export interface Configuration {
