095ed0116da58cadf636f80f7c5b16caf836642c
Panem <titoluccao@gmail.com>
2022-04-03 10:54:54 -0300

    feat: add getConfig

--- src/Config.ts
+++ src/Config.ts
@@ -1,12 +1,24 @@
+import Storage from './Storage'
+
 export default class Config {
 
     private static _instance: Config
 
-    private constructor() {}
+    private constructor() { }
 
     static getInstance(): Config {
         if (!this._instance) this._instance = new Config()
         return this._instance
     }
 
+    async getConfig(): Promise<Configuration> {
+        const storage = Storage.getInstance()
+        const collection = await storage.getCollection('settings')
+        return await storage.getDocuments<Configuration>(collection)[0]
+    }
+
+}
+
+export interface Configuration {
+    BOT_TOKEN: string
 }
