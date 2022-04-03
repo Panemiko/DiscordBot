c1002a9e0cc8e76e360ab4221e3df690a6fe19bf
Panem <titoluccao@gmail.com>
2022-04-02 22:37:03 -0300

    feat: Config validation process

--- src/Config.ts
+++ src/Config.ts
@@ -1,14 +1,27 @@
+import { config as dotenv } from 'dotenv'
 import type { FirebaseOptions } from 'firebase/app'
 
 export default class Config {
 
-    config: Configuration
+    config!: Configuration
 
     constructor() {
+        this.setupDotenv()
+        this.verifyKeys()
+        this.setupConfig()
+    }
+
+    async setupDotenv() {
+        if (process.env.NODE_ENV === 'development') dotenv()
+    }
+
+    async verifyKeys() {
         if (!process.env.FIREBASE_CONFIG) {
             throw new Error('Firebase settings not found in FIREBASE_CONFIG')
         }
+    }
 
+    async setupConfig() {
         this.config = {
             firebaseConfig: JSON.parse(process.env.FIREBASE_CONFIG)
         }
