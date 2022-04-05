b37cbecb8e8fe4c19df3a0e00ba491c67cc452a9
Panem <titoluccao@gmail.com>
2022-04-04 22:14:57 -0300

    feat: add createApplication on Storage

--- src/Application.ts
+++ src/Application.ts
@@ -1,9 +1,11 @@
 import Bot from './Bot'
+import Config from './Config'
 import Storage from './Storage'
 
 export default class Application {
     constructor() {
         Bot.getInstance()
+        Config.getInstance()
         Storage.getInstance()
     }
 
--- src/Storage.ts
+++ src/Storage.ts
@@ -1,12 +1,17 @@
-import { Firestore } from '@google-cloud/firestore'
+import type { App } from 'firebase-admin/app'
+import type { Firestore } from '@google-cloud/firestore'
+import admin from 'firebase-admin'
 import { config as dotenv } from 'dotenv'
+import { initializeApp } from 'firebase-admin/app'
 
 export default class Storage {
     private static _instance: Storage
+    private app!: admin.app.App
     private db!: Firestore
 
     private constructor() {
-        this.createDatabase(this.getDatabaseKey())
+        this.createApplication(this.getFirebaseOptions())
+        this.createDatabase()
     }
 
     static getInstance(): Storage {
@@ -24,15 +29,27 @@ export default class Storage {
             .doc(id) as unknown as FirebaseFirestore.DocumentReference<T>
     }
 
-    private getDatabaseKey(): FirebaseFirestore.Settings {
+    private getFirebaseOptions(): admin.AppOptions {
         if (process.env.NODE_ENV === 'development') dotenv()
-        if (!process.env.FIREBASE_KEY)
+        if (
+            !process.env.FIREBASE_CREDENTIALS ||
+            !process.env.FIREBASE_DATABASE_URL
+        )
             throw new Error('Firebase config not found')
 
-        return JSON.parse(process.env.FIREBASE_KEY)
+        return {
+            credential: admin.credential.cert(
+                JSON.parse(process.env.FIREBASE_CREDENTIALS)
+            ),
+            databaseURL: process.env.FIREBASE_DATABASE_URL,
+        }
     }
 
-    private createDatabase(key: FirebaseFirestore.Settings) {
-        this.db = new Firestore(key)
+    private createApplication(options: admin.ServiceAccount): void {
+        this.app = admin.initializeApp(options)
+    }
+
+    private createDatabase() {
+        this.db = this.app.firestore()
     }
 }
